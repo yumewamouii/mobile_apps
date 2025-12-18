5506e8fce76d05402e31708734e3f5ea92161690
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2025-12-19 03:42:39 +0800

    did task tabactivity

---
+++ tabactivity/app/src/main/java/com/example/tabs/CurrentTaskFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tabs
+
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CurrentTaskFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_current_task, container, false)
+        view.setBackgroundColor(Color.BLUE)
+        return view
+        //return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ tabactivity/app/src/main/java/com/example/tabs/FinishTaskFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tabs
+
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FinishTaskFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_finish_task, container, false)
+        view.setBackgroundColor(Color.YELLOW)
+        return view
+        //return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
